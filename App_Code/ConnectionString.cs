94583aac270a70bc37aabd39ab2ab518759f2cdd
Pinky Raj <95484162+Pinkyraj9977@users.noreply.github.com>
2023-07-02 21:51:43 +0530

    Add files via upload

--- App_Code/ConnectionString.cs
+++ App_Code/ConnectionString.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
 
+
+/// <summary>
+/// Summary description for ConnectionString
+/// </summary>
+public class ConnectionString
+{
+	public ConnectionString()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public  static SqlConnection getConnection()
+    {
+      SqlConnection c = new SqlConnection("Data Source=PINKY\\SQLEXPRESS; Initial Catalog=MyDB; User Id=sa; Password=Abc123");
+        return c;
+    }
+
+}
